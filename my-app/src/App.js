2f1f906d075f23174d5d0209441e3d17afd388a6
Joseph Jin <jerkjoe61@gmail.com>
2018-12-05 14:51:53 -0800

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,18 +16,3 @@ class App extends React.Component {
 
 export default App
 
-
-<script>
-   if (typeof(stockdio_events) == "undefined") {
-      stockdio_events = true;
-      var stockdio_eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-      var stockdio_eventer = window[stockdio_eventMethod];
-      var stockdio_messageEvent = stockdio_eventMethod == "attachEvent" ? "onmessage" : "message";
-      stockdio_eventer(stockdio_messageEvent, function (e) {
-         if (typeof(e.data) != "undefined" && typeof(e.data.method) != "undefined") {
-            eval(e.data.method);
-         }
-      },false);
-   }
-</script>
-<iframe id='st_87db05c95c50468ca38a51370cf1d612' frameBorder='0' scrolling='no' width='100%' height='100%' src='https://api.stockdio.com/visualization/financial/charts/v1/Ticker?app-key=896EDD90C19E4295B1C85CC8D2179D10&stockExchange=CSE&symbols=CSI;OTCMKTS%3ACADMF&palette=Financial-Light&layoutType=3&onload=st_87db05c95c50468ca38a51370cf1d612'></iframe>
