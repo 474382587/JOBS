289ecbc5f6b53c494a0158e0790aa44196739b7f
Joseph Jin <jerkjoe61@gmail.com>
2018-12-28 15:04:11 -0800

    display chat list

--- my-app/src/container/Message/Message.js
+++ my-app/src/container/Message/Message.js
@@ -1,16 +1,49 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { List } from 'antd-mobile'
 
 class Msg extends React.Component {
+    getLast(arr) {
+        return arr[arr.length - 1]
+    }
     render() {
-        console.log(this.props)
+        // if(!this.props.chat.chatmsg.length) {
+        //     return
+        // }
+
+        const Item = List.Item
+        const Brief = Item.Brief
+        const userid = this.props.user._id
+        const userinfo = this.props.chat.users
+        // console.log(this.props)
         const msgGroup = {}
         this.props.chat.chatmsg.forEach(e => {
             msgGroup[e.chatid] = msgGroup[e.chatid] || []
             msgGroup[e.chatid].push(e)
         })
-        console.table(msgGroup)
-        return <div>Message Center</div>
+        // console.table(msgGroup)
+        const chatList = Object.values(msgGroup)
+        console.log(chatList)
+        return (
+            <div>
+                <List>
+                    {chatList.map(e => {
+                        const lastItem = this.getLast(e)
+                        const targetId = e[0].from === userid ? e[0].to : e[0].from
+                        const name = userinfo[targetId] && userinfo[targetId].name
+                        const avatar = userinfo[targetId] && userinfo[targetId].avatar
+                        return (
+                            <Item key={lastItem._id} thumb={require("../../components/img/"+ avatar +".png")}>
+                                {lastItem.content}
+                                <Brief>{
+                                    name
+                                }</Brief>
+                            </Item>
+                        )
+                    })}
+                </List>
+            </div>
+        )
     }
 }
 
